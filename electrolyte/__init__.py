0328286edef4c0684a6efdbed68108d5d8cdd889
Laurent <laurent@adyoulike.com>
2015-03-24 20:48:17 +0100

    FIX Json serialization

--- electrolyte/__init__.py
+++ electrolyte/__init__.py
@@ -4,11 +4,30 @@
 """Main entry point
 """
 from pyramid.config import Configurator
+from pyramid.renderers import JSON
+import json
 import os
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include("cornice")
+    config.add_renderer('jsonplusplus', JSON(serializer=renderer_json_default))
     config.scan("electrolyte.views")
     config.add_static_view(name='', path=os.path.join(os.path.dirname(__file__), '..', 'static'), cache_max_age=0)
     return config.make_wsgi_app()
+
+def renderer_json_default(obj, **ka):
+    return json.dumps(obj, default=json_dumps_default)
+
+def json_dumps_default(obj):
+    """Default JSON serializer."""
+    import calendar, datetime
+
+    if isinstance(obj, datetime.datetime):
+        if obj.utcoffset() is not None:
+            obj = obj - obj.utcoffset()
+    millis = int(
+        calendar.timegm(obj.timetuple()) * 1000 +
+        obj.microsecond / 1000
+    )
+    return millis
