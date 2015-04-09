94b6c404e21eb80a5dc3ca749f2cf9e38f909e28
Laurent <laurent@adyoulike.com>
2015-04-09 21:40:39 +0200

    Move to description on search by barcode

--- static/app/page/search/search.js
+++ static/app/page/search/search.js
@@ -1,6 +1,6 @@
 "use strict";
 
-electrolyte.directive('elPageSearch', function($location, searchBackendService, barcodeService) {
+electrolyte.directive('elPageSearch', function($location, searchBackendService, barcodeService, nav) {
 	return {
 		restrict: 'E',
 		scope: {},
@@ -10,6 +10,7 @@ electrolyte.directive('elPageSearch', function($location, searchBackendService, 
 				barcodeService.askForBarcode("Recherche par code", function(code) {
 					// Default click behavior: go to thing
 					$location.path("/things/"+code);
+					nav.tab = nav.DESCRIPTION;
 				});
 			};
 		},
